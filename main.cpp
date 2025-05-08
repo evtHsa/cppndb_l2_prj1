1d9b79618958c9cd494535d00279ae27d1338013
Your Name <you@example.com>
2025-05-08 23:30:44 +0000

    todo 1 in main.cpp

--- main.cpp
+++ main.cpp
@@ -51,17 +51,20 @@ int main(int argc, const char **argv)
         else
             osm_data = std::move(*data);
     }
-    //Complete this TODO to satisfy Project Rubric Criterias of User Input
   
-    // TODO 1: Declare floats `start_x`, `start_y`, `end_x`, and `end_y` and get
+    //Declare floats `start_x`, `start_y`, `end_x`, and `end_y` and get
     // user input for these values using std::cin. Pass the user input to the
     // RoutePlanner object below in place of 10, 10, 90, 90.
+    float start_x, start_y, end_x, end_y;
+    std::cout << "enter blank separated start_x, start_y, end_x, end_y:";
+    std::cin >> start_x >> start_y >> end_x >> end_y;
+    std::cout << "\n\nstart_x:" << start_x << ", start_y: " << start_y << ", end_x: " << end_x << ", end_y: " << end_y << "\n";
 
     // Build Model.
     RouteModel model{osm_data};
 
     // Create RoutePlanner object and perform A* search.
-    RoutePlanner route_planner{model, 10, 10, 90, 90};
+    RoutePlanner route_planner{model, start_x, start_y, end_x, end_y};
     route_planner.AStarSearch();
 
     std::cout << "Distance: " << route_planner.GetDistance() << " meters. \n";
